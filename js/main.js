64709071bb748de28e68da8f32bcae003d5a46d3
Yu Zhangqu <yuzhangqu@qq.com>
2017-04-07 22:39:08 +0800

    more powerful

--- js/main.js
+++ js/main.js
@@ -6,6 +6,19 @@ var gIndex = 1;
 var qIndex = 1;
 var gObjs = new Array(10);
 
+function init() {
+    showTitle();
+    gObjs[1] = new Group1();
+    gObjs[2] = new Group2();
+    gObjs[3] = new Group3();
+    gObjs[4] = new Group4();
+    gObjs[5] = new Group5();
+    gObjs[6] = new Group6();
+    gObjs[7] = new Group7();
+    gObjs[8] = new Group8();
+    gObjs[9] = new Group9();
+}
+
 function repeat(begin, end, time, func) {
     return function() {
         func.call(null, begin, end);
@@ -25,6 +38,7 @@ function showQuiz(begin, end) {
     if (begin == end) {
         $(".card-text").text("");
         $("#go").removeClass("disabled");
+        $("#go_icon").removeClass("fa-spin");
         flushButton();
     } else {
         $(".card-text").number(gObjs[gIndex].nums[begin]);
@@ -48,67 +62,106 @@ function showAnswer() {
     }
 }
 
-function init() {
-    $(".card-header").text(headText + " " + gStrs[gIndex] + " " + qStrs[qIndex]);
-    gObjs[1] = new Group1();
-    gObjs[2] = new Group2();
-    gObjs[3] = new Group3();
-    gObjs[4] = new Group4();
-    gObjs[5] = new Group5();
-    gObjs[6] = new Group6();
-    gObjs[7] = new Group7();
-    gObjs[8] = new Group8();
-    gObjs[9] = new Group9();
-}
-
-function flushButton() {
-    if (gIndex <= 1) {
-        $("#prev").addClass("disabled");
-    }
-    if (gIndex < (gStrs.length - 1)) {
-        $("#next").removeClass("disabled");
-    }
-    if (gIndex > 1) {
-        $("#prev").removeClass("disabled");
-    }
-    if (gIndex >= (gStrs.length - 1)) {
-        $("#next").addClass("disabled");
-    }
-}
-
 function prevGroup() {
-    if (gIndex > 0 && !$("#prev").hasClass("disabled")) {
-        gIndex--;
-        qIndex = 1;
-        flushButton();
-        $(".card-header").text(headText + " " + gStrs[gIndex] + " " + qStrs[qIndex]);
+    if ($("#prev").hasClass("disabled")) {
+        return false;
     }
+    gIndex--;
+    qIndex = 1;
+    flushButton();
+    resumePlayButton();
+    showTitle();
+}
+
+function prevQuiz() {
+    if ($("#prev_mini").hasClass("disabled")) {
+        return false;
+    }
+    qIndex--;
+    if (qIndex < 1) {
+        gIndex--;
+        qIndex = gObjs[gIndex].mix;
+    }
+    flushButton();
+    resumePlayButton();
+    showTitle();
+}
+
+function nextQuiz() {
+    if ($("#next_mini").hasClass("disabled")) {
+        return false;
+    }
+    qIndex++;
+    if (qIndex > gObjs[gIndex].mix) {
+        gIndex++;
+        qIndex = 1;
+    }
+    flushButton();
+    resumePlayButton();
+    showTitle();
 }
 
 function nextGroup() {
-    if (gIndex < (gStrs.length - 1) && !$("#next").hasClass("disabled")) {
-        gIndex++;
-        qIndex = 1;
-        flushButton();
-        $(".card-header").text(headText + " " + gStrs[gIndex] + " " + qStrs[qIndex]);
+    if ($("#next").hasClass("disabled")) {
+        return false;
     }
+    gIndex++;
+    qIndex = 1;
+    flushButton();
+    resumePlayButton();
+    showTitle();
+}
+
+function flushButton() {
+    enableAll();
+
+    if (gIndex == 1) {
+        $("#prev").addClass("disabled");
+        if (qIndex == 1) {
+            $("#prev_mini").addClass("disabled");
+        }
+    }
+
+    if (gIndex == (gStrs.length - 1)) {
+        $("#next").addClass("disabled");
+        if (qIndex == gObjs[gIndex].mix) {
+            $("#next_mini").addClass("disabled");
+        }
+    }
+}
+
+function disableAll() {
+    $("#prev").addClass("disabled");
+    $("#prev_mini").addClass("disabled");
+    $("#go").addClass("disabled");
+    $("#next_mini").addClass("disabled");
+    $("#next").addClass("disabled");
+}
+
+function enableAll() {
+    $("#prev").removeClass("disabled");
+    $("#prev_mini").removeClass("disabled");
+    $("#go").removeClass("disabled");
+    $("#next").removeClass("disabled");
+    $("#next_mini").removeClass("disabled");
+}
+
+function resumePlayButton() {
+    $("#go_icon").removeClass("fa-refresh");
+    $("#go_icon").addClass("fa-play-circle-o");
+}
+
+function showTitle() {
+    $(".card-header").text(headText + " " + gStrs[gIndex] + " " + qStrs[qIndex]);
 }
 
 function go() {
     if ($("#go").hasClass("disabled")) {
         return false;
     }
-    if (qIndex > gObjs[gIndex].mix && gIndex < (gStrs.length - 1)) {
-        nextGroup();
-    }
-    if (qIndex > gObjs[gIndex].mix && gIndex >= (gStrs.length - 1)) {
-        $(".card-text").text("比赛结束");
-    } else {
-        $(".card-header").text(headText + " " + gStrs[gIndex] + " " + qStrs[qIndex]);
-        gObjs[gIndex].generate(qIndex++);
-        $("#prev").addClass("disabled");
-        $("#go").addClass("disabled");
-        $("#next").addClass("disabled");
-        setTimeout(repeat(0, 3, 1000, showCount), 0);
-    }
+    gObjs[gIndex].generate(qIndex);
+    disableAll();
+    $("#go_icon").removeClass("fa-play-circle-o");
+    $("#go_icon").addClass("fa-refresh fa-spin");
+    setTimeout(repeat(0, 3, 1000, showCount), 0);
 }
